71d6ef29d2041b4590f047d927095098a97289e5
Karl Spiteri <karl.spiteri.e21724@mcast.edu.mt>
2022-12-01 23:02:35 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include "mbed.h"
+#include "math.h"
+#define N 1000
+#define PI 3.142
+
+AnalogOut aout(A5);
+
+int n;
+float sinW[N];
 
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
+    float freq = 1.0;
+    float amp  = 1.0;
+    float T = 1.0/freq;
+    float ts = T/N;
 
+    for (n=0; n<N; n++) {
+	    sinW[n]=sin(0.303*amp*2*PI*freq*(float)n*ts);            
+    }  
+
+    while (true) 
+    {
+        for (n=0; n<N; n++) {
+            aout.write(0.5 + sinW[n]); //0.5 is dc offset the 1.6V offset in the graph on the whiteboard
+            wait_us((int)ts*1000000);
+        }         
     }
 }
 
